d037990434a151b3abdc2b926599f80f787c68e3
Gabriel Moreira <gabrielmoreirafermino@hotmail.com>
2025-05-20 17:21:04 -0300

    Primeiro Commit

---
+++ src/TiposDeDados/DadosNaoPrimitivos.java
@@ -0,0 +1,24 @@
+package TiposDeDados;
+
+import java.util.Locale;
+
+public class DadosNaoPrimitivos {
+    public static void main(String[] args) {
+
+        /*Dados não primitivos: String, Array, Class, enum
+        * Criar um ninja e atribuir metodos a ele!
+        * */
+
+        String nome="Naruto Uzumaki";
+        String nomeUpperCase = nome.toUpperCase(); //Tudo em Capslock
+
+        System.out.println("Caixa alta: " + nomeUpperCase);
+        System.out.println("Caixa Normal: " + nome);
+
+        //Aldeia do naruto
+        String aldeia="Aldeia da Folha";
+        String aldeialower=aldeia.toLowerCase(); // caixa baixa
+
+        System.out.println("Normal: " + aldeialower);
+    }
+}
---
+++ src/TiposDeDados/DadosPrimitivos.java
@@ -0,0 +1,20 @@
+package TiposDeDados;
+
+public class DadosPrimitivos {
+    public static void main(String[] args) {
+        /*Criar um Ninja - Naruto | Dados Primitivos*/
+
+
+        int Idade=16;
+        double Altura=1.65;
+        char Inicial='N';
+        boolean vivoOuMorto = false;
+        Long SaldoBancario = 999999L;
+
+        System.out.println("Minha inicial: " + Inicial);
+        System.out.println("Minha idade é: " + Idade); //Comando para mostrar no terminal
+        System.out.println("Estou vivo: " + vivoOuMorto);
+        System.out.println("SaldoBancario é " + SaldoBancario);
+
+    }
+}
