78699568815d05d61dcc763dc469e0bf78864bc7
Gabriel Moreira <gabrielmoreirafermino@hotmail.com>
2025-06-26 18:19:09 -0300

    Adicionada aula de Ternarios!

---
+++ src/Condicionais/Ternarios.java
@@ -0,0 +1,18 @@
+package Condicionais;
+import java.util.Scanner;
+
+public class Ternarios {
+    public static void main(String[] args) {
+
+        /*
+        * Ternarios: Maneiras de reduzir o codigo
+        * Usado como If e Else
+        * variavel = (condicao) ? ValorVerdadeiro : ValorFalso;
+        * */
+
+        short numeroDeMissoes = 10;
+        String nivelDoNinja = (numeroDeMissoes >= 10) ? "Esse ninja esta com mais de 10 missões": "Esse ninja tem menos de 10 missões";
+        System.out.println(nivelDoNinja);
+
+    }
+}
