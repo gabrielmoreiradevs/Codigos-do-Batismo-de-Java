4fdc7453e4f9f2c9ac11332f8b4b4e68cfe65ac9
Gabriel Moreira <gabrielmoreirafermino@hotmail.com>
2025-06-30 19:29:18 -0300

    Adicionada aula de Array.java e o package Array!

---
+++ src/Arrays/Array.java
@@ -0,0 +1,23 @@
+package Arrays;
+
+public class Array {
+    public static void main(String[] args) {
+
+        //Tipo[] nomeDaVariavel = new Tipo[qtdElementos]
+        //Tipo[] nomeDaVariavel = {Conteudo};
+
+        String[] ninja= new String[4];
+        ninja[0] = "Naruto";
+        ninja[1] = "Sasuke";
+        ninja[2] = "Sakura";
+        ninja[3] = "Hinata";
+
+        String[] ninjaA ={"Naruto","Sakura","Hinata","Sasuke"};
+
+        System.out.println(ninja);
+
+
+
+
+    }
+}
